7587ab2eb02a3210bbd7a5fbf32e88866e22248c
unknown <nafilahnursabila443@gmail.com>
2025-03-26 14:33:32 +0700

    menambahkan fitur perhitungan keliling persegi

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-function calculateArea() {
+function halamanLuas(){
+    window.location.href = "index.html";
+}
+
+function halamanKeliling(){
+    window.location.href = "keliling.html";
+}
+
+function hitungLuas() {
     const sisi = document.getElementById('sisi').value;
     const luas = sisi * sisi;
     document.getElementById('hasil').innerText = `L = S x S 
@@ -8,7 +16,22 @@ function calculateArea() {
     document.getElementById('result').innerText = `L = ${luas}`;*/
 }
 
-function reset() {
+function resetLuas() {
+    document.getElementById('sisi').value = '';
+    document.getElementById('hasil').innerText = '';
+}
+
+function hitungKeliling() {
+    const sisi = document.getElementById('sisi').value;
+    const keliling = 4 * sisi;
+    document.getElementById('hasil').innerText = `K = 4 x S 
+    K = 4 x ${sisi} 
+    K = ${keliling}`;
+    /*document.getElementById('sisi').innerText = `L = ${sisi} x ${sisi}`;
+    document.getElementById('result').innerText = `L = ${luas}`;*/
+}
+
+function resetKeliling() {
     document.getElementById('sisi').value = '';
     document.getElementById('hasil').innerText = '';
 }
