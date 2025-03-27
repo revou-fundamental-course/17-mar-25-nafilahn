e02cb16af301e4e37a9041cd2f7d55e52d12f8fd
unknown <nafilahnursabila443@gmail.com>
2025-03-27 20:43:22 +0700

    menambahkan fitur luas dan keliling persegi panjang

--- js/script.js
+++ js/script.js
@@ -1,37 +1,100 @@
-function halamanLuas(){
+//fungsi untuk menampilkan bagian luas persegi
+function luas_persegi(){
     window.location.href = "index.html";
 }
 
-function halamanKeliling(){
+//fungsi untuk menampilkan bagian keliling persegi
+function keliling_persegi(){
     window.location.href = "keliling.html";
 }
 
-function hitungLuas() {
-    const sisi = document.getElementById('sisi').value;
-    const luas = sisi * sisi;
-    document.getElementById('hasil').innerText = `L = S x S 
-    L = ${sisi} x ${sisi} 
-    L = ${luas}`;
-    /*document.getElementById('sisi').innerText = `L = ${sisi} x ${sisi}`;
-    document.getElementById('result').innerText = `L = ${luas}`;*/
+//fungsi untuk menampilkan bagian luas persegi panjang
+function luas_pp(){
+    window.location.href = "luaspp.html";
 }
 
-function resetLuas() {
-    document.getElementById('sisi').value = '';
-    document.getElementById('hasil').innerText = '';
+//fungsi untuk menampilkan bagian keliling persegi panjang
+function keliling_pp(){
+    window.location.href = "kelilingpp.html";
 }
 
-function hitungKeliling() {
-    const sisi = document.getElementById('sisi').value;
-    const keliling = 4 * sisi;
-    document.getElementById('hasil').innerText = `K = 4 x S 
-    K = 4 x ${sisi} 
-    K = ${keliling}`;
-    /*document.getElementById('sisi').innerText = `L = ${sisi} x ${sisi}`;
-    document.getElementById('result').innerText = `L = ${luas}`;*/
+//fungsi untuk menghitung luas persegi
+function hitung_luas(){
+    const sisi = document.getElementById('sisi').value; //ambil nilai sisi dari input
+    if (sisi > 0){
+        const luas = sisi * sisi; //hitung luas
+        document.getElementById('hasil').innerText = `L = S x S 
+        L = ${sisi} x ${sisi} 
+        L = ${luas}`; //menampilkan hasil
+    }else{
+        alert("Silakan masukkan nilai yang valid!"); //pesan kesalahan
+    }
 }
 
-function resetKeliling() {
-    document.getElementById('sisi').value = '';
-    document.getElementById('hasil').innerText = '';
+//fungsi untuk mereset perhitungan luas persegi
+function reset_luas() {
+    document.getElementById('sisi').value = ''; //kosongkan inputan
+    document.getElementById('hasil').innerText = ''; //kosongkan hasil
+}
+
+//fungsi untuk menghitung keliling persegi
+function hitung_keliling() {
+    const sisi = document.getElementById('sisi').value; //ambil nilai sisi dari input
+    if (sisi > 0){
+        const keliling = 4 * sisi; //hitung keliling
+        document.getElementById('hasil').innerText = `K = 4 x S 
+        K = 4 x ${sisi} 
+        K = ${keliling}`; //menampilkan hasil
+    }else{
+        alert("Silakan masukkan nilai yang valid!"); //pesan kesalahan
+    }
+}
+
+//fungsi untuk mereset perhitungan keliling persegi
+function reset_keliling() {
+    document.getElementById('sisi').value = ''; //kosongkan inputan
+    document.getElementById('hasil').innerText = ''; //kosongkan hasil
+}
+
+//fungsi untuk menghitung luas persegi panjang
+function hitung_luas_pp() {
+    const panjang = document.getElementById('panjang').value; //ambil nilai panjang dari input
+    const lebar = document.getElementById('lebar').value; //ambil nilai lebar dari input
+    if (panjang > 0 && lebar > 0){
+        const luas = panjang *lebar; //hitung luas
+        document.getElementById('hasil').innerText = `L = P x L 
+        L = ${panjang} x ${lebar} 
+        L = ${luas}`; //menampilkan hasil
+    }else{
+        alert("Silakan masukkan nilai yang valid!"); //pesan kesalahan   
+    }
+    
+}
+
+//fungsi untuk mereset perhitungan luas persegi panjang
+function reset_luas_pp() {
+    document.getElementById('panjang').value = ''; //kosongkan inputan
+    document.getElementById('lebar').value = ''; //kosongkan inputan
+    document.getElementById('hasil').innerText = ''; //kosongkan hasil
+}
+
+//fungsi untuk menghitung keliling persegi panjang
+function hitung_keliling_pp() {
+    const panjang = document.getElementById('panjang').value; //ambil nilai panjang dari input
+    const lebar = document.getElementById('lebar').value; //ambil nilai lebar dari input
+    if (panjang > 0 && lebar > 0){
+        const keliling = 2 * panjang + 2 * lebar; //hitung luas
+        document.getElementById('hasil').innerText = `K = 2P + 2L
+        K = 2 x ${panjang} + 2 x ${lebar}
+        K = ${keliling}`; //menampilkan hasil
+    }else{
+        alert("Silakan masukkan nilai yang valid!"); //pesan kesalahan
+    }
+}
+
+//fungsi untuk mereset perhitungan keliling persegi panjang
+function reset_keliling_pp() {
+    document.getElementById('panjang').value = ''; //kosongkan inputan
+    document.getElementById('lebar').value = ''; //kosongkan inputan
+    document.getElementById('hasil').innerText = ''; //kosongkan hasil
 }
